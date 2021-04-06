01f6d0ec0514f9de804077c74fe4043112a258f8
unknown <you@example.com>
2021-04-06 17:45:15 +0200

    countries AJAX finished no bonus

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ $(function(){
         
         $.ajax({
             url: 'http://localhost:8001/countries', 
-            success: function(data, status, response){
-                return data
+            success: (data, status, response) => {
+                // return data
+                $("#country-1").html(data[0])
+                $("#country-2").html(data[1])
+                $("#country-3").html(data[2])
+                $("#country-4").html(data[3])
+                $("#country-5").html(data[4])
+                // $("#okkk").html(data)
             }
         })
+        
 
-        $("#country-1").html()
-        $("#country-2").html()
-        $("#country-3").html()
-        $("#country-4").html()
-        $("#country-5").html()
 
         
     })
